853a4247b55b46afb037d3922353be40c69e14cb
Nguyen Thanh Binh <binh.drca@gmail.com>
2013-07-16 17:04:17 +0700

    Add sample folder (v 1.1.7)

---
+++ sample/builder/build.bat
@@ -0,0 +1,2 @@
+adt -package -target ipa-test -provisioning-profile "03_Certificates\Provisioning_Profile\2013_07_03_DRCH_GENERAL.mobileprovision" -storetype pkcs12 -keystore "03_Certificates\Digital_Signature\Drcomhealth_2015.p12" -storepass 123456 "MARCAIN.ipa" "MERONEM-app.xml" "MERONEM.swf" "Default.png" "AppIconsForPublish"
+pause
